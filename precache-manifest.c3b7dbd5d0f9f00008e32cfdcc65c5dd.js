c875a50bcbbfeb70a0b08d909c77f5f2706103d8
Hrishikesh Hasnale <hrishikesh.hasnale@workplaceoptions.com>
2020-07-17 13:17:59 +0530

    Updates

---
+++ precache-manifest.c3b7dbd5d0f9f00008e32cfdcc65c5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957874289a90cf4a8160d2176bfa83b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bcb1683941affaa9c22e",
+    "url": "/monsters-rolodex/static/css/main.a5a7a0b4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb1683941affaa9c22e",
+    "url": "/monsters-rolodex/static/js/main.940d7614.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
